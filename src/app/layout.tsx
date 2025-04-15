6f43fa631dce3ebdfcb5742fc84aca00ce2fd8a8
Maria Po <mariapodobrazhnykh@gmail.com>
2025-04-15 08:58:07 +0200

    Revert "Обновление шрифтов и удаление описаний изображений"
    This reverts commit b14d7a208458c5590ef612a7f7d80401562f66fd.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,6 @@ export default function RootLayout({
     <html lang="ru">
       <head>
         <link rel="icon" href="/main/Group_1321315021.png" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Caladea:ital,wght@0,400;0,700;1,400;1,700&family=Onest:wght@100..900&display=swap" rel="stylesheet" />
       </head>
       <body>
         {/* SVG иконки для курсора */}
